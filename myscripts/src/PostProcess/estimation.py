df9e32b933b8d704441a156af1217ddc18cc05c8
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2024-02-05 11:11:36 -0500

    rmv some files

--- myscripts/src/PostProcess/estimation.py
+++
@@ -1,5 +0,0 @@
-# A generic script where given a property(s), a method to estimate the property and 
-# existing data, determine if estimate is sufficient or need to continue simulation
-
-# PIPEDREAM: Given an existing Pilot/WorkFlow object, this script can determine how much more work in that workflow
-# Or what parts of the workflow are needed to get an estimate within a certain confidence threshold 
